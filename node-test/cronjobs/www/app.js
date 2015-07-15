41cc6590ae7ebceb5088373dbd37724b15e7be6b
Adam Johnstone <ajjoh5@gmail.com>
2015-07-16 06:41:01 +1000

    Node-Test: - Creating UI for NodeJobs

---
+++ node-test/cronjobs/www/app.js
@@ -0,0 +1,43 @@
+angular.module('app', ['ngRoute', 'app.controllers'])
+
+    .config(function($routeProvider) {
+
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'cronjobs/pages/home.html',
+                controller  : 'homeController'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl : 'cronjobs/pages/about.html',
+                controller  : 'aboutController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'cronjobs/pages/contact.html',
+                controller  : 'contactController'
+            });
+    });
+
+angular.module('app.controllers', [])
+
+    .controller('homeController', function($scope) {
+
+        $scope.message = 'NodeFire.io';
+
+    })
+
+    .controller('aboutController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'about ctrl...';
+    })
+
+    .controller('contactController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+        $scope.contactMessage = "This is a contact message";
+    });
--- node-test/server.js
+++ node-test/server.js
@@ -1,92 +1,74 @@
 var fs = require('fs');
 var path = require('path');
-var Hapi = require('hapi');
-var hotswap = require('hotswap');
-
-var jobIDPool = 0;
-var jobs = [];
-
-//Monitor and Hot Swap changed files
-hotswap.on('swap', function() {
-    console.log('Swapped...');
-});
-
-//Cron Jobs -- cronTime: '*/5 * * * * *',
-var CronJob = require('cron').CronJob;
-
-var server = new Hapi.Server();
-server.connection({
-    host: 'localhost',
-    port: 3001
-});
+var express = require('express');
+var exphbs = require('express-handlebars');
 
 
-//Handle New Cron Job Route
-server.route({
-    method: 'GET',
-    path: '/AddJob/{filename}',
-    handler: function (request, reply) {
+//Create & Configure express app
+var app = express();
+hbs = exphbs.create({
+    extname:'handlebars',
+    defaultLayout: 'layout',
+    partialsDir: "views/partials/",
+    layoutsDir: "views/layouts/",
+    helpers: {
+        block: function (name) {
+            var blocks  = this._blocks,
+                content = blocks && blocks[name];
 
-        var filename = request.params.filename;
-        console.log('Adding job to array...');
+            return content ? content.join('\n') : null;
+        },
 
-        var cronJob = {
-            id : jobIDPool++,
-            job : null,
-            isActive : false,
-            isRunning : false
-        };
+        contentFor: function (name, options) {
+            var blocks = this._blocks || (this._blocks = {}),
+                block  = blocks[name] || (blocks[name] = []);
 
-        var cJob = new CronJob({
-            cronTime: '*/5 * * * * 1-5',
-            onTick: function () {
-                var thisJobID = cronJob.id;
-                console.log('CronJob #: ' + thisJobID);
-                jobs[thisJobID].isRunning = true;
-
-                //Load in the file dynamically, then require it
-                var jobsDir = __dirname + '/jobs/';
-                var jobFile = path.join(jobsDir, filename);
-                console.log(jobFile);
-
-                var reqJobFile = require(jobFile);
-                reqJobFile.Execute(thisJobID);
-
-                //var currTime = new Date();
-                //console.log('[5s] Interval Hit: ' + currTime);
-            },
-            onComplete: function() {
-                console.log('Finished Job...');
-            },
-            start: false,
-            timeZone: "Australia/Melbourne"
-        });
-
-        cronJob.job = cJob;
-
-        jobs.push(cronJob);
-
-        reply('Added Job #:' + cronJob.id);
+            block.push(options.fn(this));
+        }
     }
 });
 
-server.route({
-    method: 'GET',
-    path: '/StartJob/{id}',
-    handler: function (request, reply) {
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-        var jobID = request.params.id;
-        jobs[jobID].isActive = true;
-        jobs[jobID].isRunning  = false;
-        jobs[jobID].job.start();
-
-        console.log('Starting Job #: ' + jobID);
-        reply('Starting Job #: ' + jobID);
+//Utilities
+function removeLastSlash (myUrl)
+{
+    if (myUrl.substring(myUrl.length-1) === '/') {
+        myUrl = myUrl.substring(0, myUrl.length-1);
     }
+
+    return myUrl;
+}
+
+var spa = {
+    id : 'cronjobs',
+    filepath : __dirname + '/cronjobs'
+}
+
+//app.get('/api/?*', function (req, res) {
+//    //console.log(req.path);
+//    //console.log(req.params);
+//    var params = removeLastSlash(req.params[0]).split('/');
+//    console.log(params);
+//    //res.send('Multi Segment Routes...');
+//    res.render(__dirname + '/jobs/test.handlebars', { layout: 'main2' });
+//});
+
+app.get('/', function(req, res) {
+    //var params = removeLastSlash(req.params[0]).split('/');
+
+    res.render(spa.filepath + '/home', {
+        layout : spa.filepath + '/layouts/layout',
+        spa: spa,
+        title: 'NodeJobs.UI'
+    });
 });
 
+//Static Routes for Files
+app.use('/cronjobs', express.static('cronjobs/www'));
+app.use('/js', express.static('www/js'));
 
-// Start the server
-server.start(function () {
-    console.log('Server running at:', server.info.uri);
+app.listen(3001, function() {
+    console.log('Server running at: 3001');
 });
