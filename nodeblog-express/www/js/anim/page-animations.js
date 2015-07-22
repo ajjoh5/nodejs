4ade2554cf1834eba09a19c9950fa5c0f0d20543
Adam Johnstone <adam.johnstone@igeniusgroup.com>
2015-07-22 18:06:31 +1000

    Node Blog Express: - Added animations and took out angularness

---
+++ nodeblog-express/www/js/anim/page-animations.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $(".view-slide-in").animate({
+        opacity: 1.0,
+        top: "+=10"
+    }, 800, function() {
+        // Animation complete.
+    });
+
+});
