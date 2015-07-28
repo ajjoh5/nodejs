3c0e9b4ecb41745b044b8683c8e445185ec6b2f7
Adam Johnstone <adam.johnstone@igeniusgroup.com>
2015-07-28 14:41:00 +1000

    NBE: - Added public folder for split testing performance

---
+++ nodeblog-express/public/js/anim/page-animations.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $(".view-slide-in-2").animate({
+        opacity: 1.0,
+        top: "+=10"
+    }, 800, function() {
+        // Animation complete.
+    });
+
+});
