af9c56888865a9fdf5ecc70967f4af79d261c28e
Adam Johnstone <adam.johnstone@igeniusgroup.com>
2015-08-25 14:18:51 +1000

    Node-oauth: - Added "run once" functionality to ensure method doesn't run multiple times - Added app config file to log also when last run happened for a particular range

--- nodeoauth/server.js
+++ nodeoauth/server.js
@@ -32,6 +32,85 @@ function allowCrossSiteMiddleware(req, res, next) {
     next();
 }
 
+function ensureRunOnce(req, res, next) {
+
+    console.log('[ Run Once ]');
+    var appConfigFile = path.join(__dirname + '/config/app.json');
+    var appConfigJsonFile = fs.readFileSync(appConfigFile);
+    var appJson = JSON.parse(appConfigJsonFile);
+
+    if(!appJson) {
+        res.status(500).send('ERROR: Could not load app config file.');
+    }
+    else {
+        if(!appJson.IsRunning) {
+            appJson.IsRunning = true;
+            fs.writeFileSync(appConfigFile, JSON.stringify(appJson));
+            next();
+        }
+        else {
+            res.status(500).send('ERROR: Application is already running. Please try again later.');
+        }
+    }
+}
+
+function updateRunOnceToFalse(rangeName) {
+
+    console.log('[ Upated Run Once | Range: ' + rangeName + ' ]');
+    var appConfigFile = path.join(__dirname + '/config/app.json');
+    var appConfigJsonFile = fs.readFileSync(appConfigFile);
+    var appJson = JSON.parse(appConfigJsonFile);
+
+    if(!appJson) {
+        console.log('ERROR: Could not update app config for range: ' + rangeName)
+    }
+    else {
+        var dateStamp = formatDate(new Date());
+
+        var rangeConfig = _.find(appJson.Ranges, function(item) {
+            return item.Name === rangeName;
+        });
+
+        if(!rangeConfig) {
+            rangeConfig = {
+                Name: rangeName,
+                LastRun : dateStamp
+            };
+
+            appJson.Ranges.push(rangeConfig);
+        }
+
+        //Update to running = false, LastRun for range to current date and time
+        appJson.IsRunning = false;
+        rangeConfig.LastRun = dateStamp;
+
+        fs.writeFileSync(appConfigFile, JSON.stringify(appJson));
+    }
+}
+
+function formatDate(date) {
+    var year = date.getFullYear(),
+        month = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1), // months are zero indexed
+        day = date.getDate(),
+        hour = date.getHours(),
+        minute = date.getMinutes(),
+        second = date.getSeconds(),
+        hourFormatted = hour % 12 || 12, // hour returned in 24 hour format
+        minuteFormatted = minute < 10 ? "0" + minute : minute,
+        morning = hour < 12 ? "am" : "pm";
+
+    if(month.length < 2){
+        month = '0' + month;
+    }
+
+    if(day.length < 2){
+        day = '0' + day;
+    }
+
+    //return year + "-" + month + "-" + day + "-" + hour + minuteFormatted;
+    return '' + year + month + day + "-" + hour + minuteFormatted;
+};
+
 app.get('/runway/packages/:range', allowCrossSiteMiddleware, function(req, res) {
 
     console.time('total-time');
@@ -57,7 +136,7 @@ app.get('/runway/packages/:range', allowCrossSiteMiddleware, function(req, res) 
     });
 });
 
-app.get('/runway/cache-packages/:range', function(req, res) {
+app.get('/runway/cache-packages/:range', ensureRunOnce, function(req, res) {
 
     console.time('total-time');
     console.time("get-packages");
@@ -171,29 +250,6 @@ app.get('/runway/cache-packages/:range', function(req, res) {
                         //console.log(delta[0]);
 
                         //Save location to temp location, before performing diff check
-                        function formatDate(date) {
-                            var year = date.getFullYear(),
-                                month = date.getMonth() + 1, // months are zero indexed
-                                day = date.getDate(),
-                                hour = date.getHours(),
-                                minute = date.getMinutes(),
-                                second = date.getSeconds(),
-                                hourFormatted = hour % 12 || 12, // hour returned in 24 hour format
-                                minuteFormatted = minute < 10 ? "0" + minute : minute,
-                                morning = hour < 12 ? "am" : "pm";
-
-                            if(month.length < 2){
-                                month = '0' + month;
-                            }
-
-                            if(day.length < 2){
-                                day = '0' + day;
-                            }
-
-                            //return year + "-" + month + "-" + day + "-" + hour + minuteFormatted;
-                            return '' + year + month + day + "-" + hour + minuteFormatted;
-                        };
-
                         var dateStamp = formatDate(new Date());
                         var backupFile = path.join(__dirname + '/json-files/backup/' + dateStamp + '_' + rangeName + '-cache.json');
 
@@ -217,6 +273,7 @@ app.get('/runway/cache-packages/:range', function(req, res) {
 
                     //show view of json data
                     res.send(data);
+                    updateRunOnceToFalse(rangeName);
                 });
 
             });
@@ -227,227 +284,227 @@ app.get('/runway/cache-packages/:range', function(req, res) {
 
 });
 
-app.use(proxy2('localhost:3001', '/foo'));
-
-app.use(proxy2('localhost:3001', {
-    prefix : '/food'
-    // /foo/test => /test
-    // /test => ignored by proxy
-}));
-
-app.use('/api/2', proxy('webapi.henley.com.au/jobapi', {
-    forwardPath: function(req, res) {
-        return require('url').parse(req.url).path;
-    }
-}));
-
-app.use('/api/3', proxy('http://localhost:3001/', {
-    filter: function(req, res) {
-        return req.method == 'GET';
-    },
-    forwardPath: function(req, res) {
-        return require('url').parse(req.url).path;
-    }
-}));
-
-app.get('/proxyapp/3/web', function(req, res) {
-
-    var apiUrl = 'http://webapi.henley.com.au/jobapi/Jobs/301605?keytoken=68656e6c65792061646d696e20636f6d70616e792064617461';
-
-    var options = {
-        url: apiUrl
-    };
-
-    request.get(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log('success...');
-            //console.log(response.statusCode);
-            res.send(body);
-            //console.log(body);
-        }
-        else
-        {
-            console.log('error...');
-            //console.log(error);
-            //console.log(response.statusCode);
-            console.log(body);
-            res.send(body);
-        }
-    });
-
-});
-
-app.get('/proxyapp/4/web', function(req, res) {
-
-    var apiUrl = 'http://webapi.henley.com.au/jobapi';
-    //var apiUrl = 'http://localhost:3001';
-
-    var options = {
-        url: apiUrl
-    };
-
-    request.get(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log('success...');
-            //console.log(response.statusCode);
-            res.send(body);
-            //console.log(body);
-        }
-        else
-        {
-            console.log('error...');
-            //console.log(error);
-            //console.log(response.statusCode);
-            console.log(body);
-            res.send(body);
-        }
-    });
-
-});
-
-app.get('/proxyapp/5/web', function(req, res) {
-
-    //modify the url in any way you want
-    var apiUrl = 'http://webapi.henley.com.au/jobapi/Jobs/301605?keytoken=68656e6c65792061646d696e20636f6d70616e792064617461';
-    //var newurl = 'http://localhost:3001';
-    request(apiUrl).pipe(res);
-
-});
-
-app.get('/runway/c', function(req, res) {
-
-    console.log('Runway COOKIE Auth...');
-
-    var jar = request.jar();
-    //var cookie = request.cookie('JSESSIONID');
-    //var cookie = request.cookie('JSESSIONID=4B2565E0999F804B5253DCED3BF4454C');
-
-    var data = {
-        Username : 'ajohnstone',
-        Password : 'welcome'
-    };
-
-    //jar.add(cookie);
-
-    request({
-        uri: 'http://henley.runway.com.au/actions/loginaction.jsp',
-        method: 'POST',
-        form: data,
-        jar: jar
-    }, function(error, response, body) {
-        //console.log(body);
-
-        var cjson = JSON.stringify(jar._jar);
-        var cList = JSON.parse(cjson);
-        var runwayCookie = _.find(cList.cookies, function(item){ return item.key === 'JSESSIONID'; });
-
-        //console.log(runwayCookie);
-        jsid = runwayCookie.value;
-        res.send('Completed: ' + jsid);
-    });
-
-});
-
-app.get('/runway/c/runapi', function(req, res) {
-
-    var jar = request.jar();
-    var cookie = request.cookie('JSESSIONID=' + jsid);
-
-    var apiUrl = 'https://henley.runway.com.au/api/2/packages?ModifiedSince=10/08/2015';
-    jar.setCookie(cookie, apiUrl);
-
-    var options = {
-        url: apiUrl,
-        jar: jar
-    };
-
-    //console.log(options);
-    console.log('Sending Request: ' + options.url);
-
-    request.get(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log('success...');
-            //console.log(response.statusCode);
-            res.send(body);
-            //console.log(body);
-        }
-        else
-        {
-            console.log('error...');
-            //console.log(error);
-            //console.log(response.statusCode);
-            console.log(body);
-            res.send(body);
-        }
-    });
-
-});
-
-
-app.get('/runway/oauth', function(req, res) {
-
-    console.log('Sending Runway OAUTH...');
-
-    //Your sending email address post data
-    //var sender = req.body.name + ' <' + req.body.email + '>';
-    //var mailedFrom = 'contact@adamjohnstone.co';
-    //var recipients = 'ajjoh5@gmail.com';
-    //var message = 'Contact Details: \n' + sender + '\n\nMessage:' + req.body.message;
-
-    //var options = {
-    //    auth: {
-    //        username: 'api',
-    //        password: 'key-74bcec52b1dd582c2989aec6022ce95d'
-    //    },
-    //    dataType: 'form-url-encoded'
-    //};
-
-    var options = {
-        dataType: 'form-url-encoded'
-    };
-
-
-    //var data = {
-    //    from : mailedFrom,
-    //    to : recipients,
-    //    subject : 'AdamJohnstone.co - Contact Request Submitted',
-    //    text : message
-    //};
-
-    //Authorization: Basic aGVubGV5aXQ6I2hlbjMzejIxIQ==
-
-    var data = {};
-
-    //var url = 'https://henley.runway.com.au/oauth/?client_id=0L1G4C3G7N042K91482Z4D9K3G8G&redirect_uri=http://localhost:3002/oauthcallback&response_type=code&access_type=offline&state=Y76123af';
-    var url = 'https://henley.runway.com.au/api/2/oauth/token?client_id=0L1G4C3G7N042K91482Z4D9K3G8G&';
-
-    requestify.post(url, data, options)
-    .then(function(response) {
-        // Get the response body
-        var body = response.getBody();
-        console.log(body);
-
-        res.send(body);
-    });
-
-    //requestify.get(url)
-    //.then(function(response) {
-    //    console.log('ff');
-    //    // Get the response body
-    //    var body = response.getBody();
-    //    console.log(body);
-    //    res.send('Finished');
-    //});
-
-    console.log('Finished');
-    //res.send('Finished');
-});
-
-app.get('/runway/oauthcallback', function(req, res) {
-
-    console.log('!! Received Runway OAUTH...');
-
-});
+//app.use(proxy2('localhost:3001', '/foo'));
+//
+//app.use(proxy2('localhost:3001', {
+//    prefix : '/food'
+//    // /foo/test => /test
+//    // /test => ignored by proxy
+//}));
+//
+//app.use('/api/2', proxy('webapi.henley.com.au/jobapi', {
+//    forwardPath: function(req, res) {
+//        return require('url').parse(req.url).path;
+//    }
+//}));
+//
+//app.use('/api/3', proxy('http://localhost:3001/', {
+//    filter: function(req, res) {
+//        return req.method == 'GET';
+//    },
+//    forwardPath: function(req, res) {
+//        return require('url').parse(req.url).path;
+//    }
+//}));
+//
+//app.get('/proxyapp/3/web', function(req, res) {
+//
+//    var apiUrl = 'http://webapi.henley.com.au/jobapi/Jobs/301605?keytoken=68656e6c65792061646d696e20636f6d70616e792064617461';
+//
+//    var options = {
+//        url: apiUrl
+//    };
+//
+//    request.get(options, function (error, response, body) {
+//        if (!error && response.statusCode == 200) {
+//            console.log('success...');
+//            //console.log(response.statusCode);
+//            res.send(body);
+//            //console.log(body);
+//        }
+//        else
+//        {
+//            console.log('error...');
+//            //console.log(error);
+//            //console.log(response.statusCode);
+//            console.log(body);
+//            res.send(body);
+//        }
+//    });
+//
+//});
+//
+//app.get('/proxyapp/4/web', function(req, res) {
+//
+//    var apiUrl = 'http://webapi.henley.com.au/jobapi';
+//    //var apiUrl = 'http://localhost:3001';
+//
+//    var options = {
+//        url: apiUrl
+//    };
+//
+//    request.get(options, function (error, response, body) {
+//        if (!error && response.statusCode == 200) {
+//            console.log('success...');
+//            //console.log(response.statusCode);
+//            res.send(body);
+//            //console.log(body);
+//        }
+//        else
+//        {
+//            console.log('error...');
+//            //console.log(error);
+//            //console.log(response.statusCode);
+//            console.log(body);
+//            res.send(body);
+//        }
+//    });
+//
+//});
+//
+//app.get('/proxyapp/5/web', function(req, res) {
+//
+//    //modify the url in any way you want
+//    var apiUrl = 'http://webapi.henley.com.au/jobapi/Jobs/301605?keytoken=68656e6c65792061646d696e20636f6d70616e792064617461';
+//    //var newurl = 'http://localhost:3001';
+//    request(apiUrl).pipe(res);
+//
+//});
+//
+//app.get('/runway/c', function(req, res) {
+//
+//    console.log('Runway COOKIE Auth...');
+//
+//    var jar = request.jar();
+//    //var cookie = request.cookie('JSESSIONID');
+//    //var cookie = request.cookie('JSESSIONID=4B2565E0999F804B5253DCED3BF4454C');
+//
+//    var data = {
+//        Username : 'ajohnstone',
+//        Password : 'welcome'
+//    };
+//
+//    //jar.add(cookie);
+//
+//    request({
+//        uri: 'http://henley.runway.com.au/actions/loginaction.jsp',
+//        method: 'POST',
+//        form: data,
+//        jar: jar
+//    }, function(error, response, body) {
+//        //console.log(body);
+//
+//        var cjson = JSON.stringify(jar._jar);
+//        var cList = JSON.parse(cjson);
+//        var runwayCookie = _.find(cList.cookies, function(item){ return item.key === 'JSESSIONID'; });
+//
+//        //console.log(runwayCookie);
+//        jsid = runwayCookie.value;
+//        res.send('Completed: ' + jsid);
+//    });
+//
+//});
+//
+//app.get('/runway/c/runapi', function(req, res) {
+//
+//    var jar = request.jar();
+//    var cookie = request.cookie('JSESSIONID=' + jsid);
+//
+//    var apiUrl = 'https://henley.runway.com.au/api/2/packages?ModifiedSince=10/08/2015';
+//    jar.setCookie(cookie, apiUrl);
+//
+//    var options = {
+//        url: apiUrl,
+//        jar: jar
+//    };
+//
+//    //console.log(options);
+//    console.log('Sending Request: ' + options.url);
+//
+//    request.get(options, function (error, response, body) {
+//        if (!error && response.statusCode == 200) {
+//            console.log('success...');
+//            //console.log(response.statusCode);
+//            res.send(body);
+//            //console.log(body);
+//        }
+//        else
+//        {
+//            console.log('error...');
+//            //console.log(error);
+//            //console.log(response.statusCode);
+//            console.log(body);
+//            res.send(body);
+//        }
+//    });
+//
+//});
+//
+//
+//app.get('/runway/oauth', function(req, res) {
+//
+//    console.log('Sending Runway OAUTH...');
+//
+//    //Your sending email address post data
+//    //var sender = req.body.name + ' <' + req.body.email + '>';
+//    //var mailedFrom = 'contact@adamjohnstone.co';
+//    //var recipients = 'ajjoh5@gmail.com';
+//    //var message = 'Contact Details: \n' + sender + '\n\nMessage:' + req.body.message;
+//
+//    //var options = {
+//    //    auth: {
+//    //        username: 'api',
+//    //        password: 'key-74bcec52b1dd582c2989aec6022ce95d'
+//    //    },
+//    //    dataType: 'form-url-encoded'
+//    //};
+//
+//    var options = {
+//        dataType: 'form-url-encoded'
+//    };
+//
+//
+//    //var data = {
+//    //    from : mailedFrom,
+//    //    to : recipients,
+//    //    subject : 'AdamJohnstone.co - Contact Request Submitted',
+//    //    text : message
+//    //};
+//
+//    //Authorization: Basic aGVubGV5aXQ6I2hlbjMzejIxIQ==
+//
+//    var data = {};
+//
+//    //var url = 'https://henley.runway.com.au/oauth/?client_id=0L1G4C3G7N042K91482Z4D9K3G8G&redirect_uri=http://localhost:3002/oauthcallback&response_type=code&access_type=offline&state=Y76123af';
+//    var url = 'https://henley.runway.com.au/api/2/oauth/token?client_id=0L1G4C3G7N042K91482Z4D9K3G8G&';
+//
+//    requestify.post(url, data, options)
+//    .then(function(response) {
+//        // Get the response body
+//        var body = response.getBody();
+//        console.log(body);
+//
+//        res.send(body);
+//    });
+//
+//    //requestify.get(url)
+//    //.then(function(response) {
+//    //    console.log('ff');
+//    //    // Get the response body
+//    //    var body = response.getBody();
+//    //    console.log(body);
+//    //    res.send('Finished');
+//    //});
+//
+//    console.log('Finished');
+//    //res.send('Finished');
+//});
+//
+//app.get('/runway/oauthcallback', function(req, res) {
+//
+//    console.log('!! Received Runway OAUTH...');
+//
+//});
 
 var server = app.listen(3002, function() {
     console.log('Server running at: 3002');
