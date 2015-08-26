3891d3a9c62e0e76510ba9c5255d1d3683d21b97
Adam Johnstone <ajjoh5@gmail.com>
2015-08-26 17:43:27 +1000

    n-oauth: - Added job to re cache the runway task

---
+++ nodeoauth/apps/scheduled-tasks/job-runway-recache.js
@@ -0,0 +1,22 @@
+//other plugins
+var request = require('request');
+
+function reCacheRunway(brand)
+{
+    var apiUrl = 'http://webapi.henley.com.au/rc/runway/cache-packages/' + brand;
+
+    var options = {
+        url: apiUrl
+    };
+
+    request.get(options, function (err, res, body) {
+        if (!err && res.statusCode == 200) {
+            console.log('SUCCESS: Re-cached runway with brand: "' + brand + '"');
+        }
+        else {
+            console.log('ERROR: Re-cache failed. Error was - ' + body);
+        }
+    });
+}
+
+reCacheRunway();
