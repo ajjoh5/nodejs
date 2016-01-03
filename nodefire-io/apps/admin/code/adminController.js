acd4f346064d140572eb8b94752e4c3803a53778
Adam Johnstone <ajjoh5@gmail.com>
2016-01-03 22:49:06 +1100

    Nodefire.io - Added new style sheet and html design for "admin" area - Added menu system to left hand side - Added code in index.handlebars to allow jstree to be "clickable" and redirect to new page - Added sass file to start playing with sass css compilations

--- nodefire-io/apps/admin/code/adminController.js
+++ nodefire-io/apps/admin/code/adminController.js
@@ -27,15 +27,30 @@ var adminController = function(app) {
     function listNodes(parentItem) {
         _.each(parentItem, function(item) {
             var liString = '<li>' + item.name;
+            //item type = web
             if(item.type.toLowerCase() == 'web') {
                 liString = "<li data-jstree='{\"icon\" : \"/js/icons/web_02.png\"}'>" + item.name;
             }
+
+            //item type = content-page-1
             if(item.type.toLowerCase() == 'content-page-1') {
-                liString = "<li data-jstree='{\"icon\" : \"/js/icons/doc_01.png\"}'>" + item.name;
+
+                var linkField = _.find(item.fields, function(field) {
+                    if(field.type.toLowerCase() == 'link' && field.value) {
+                        return field;
+                    }
+                });
+
+                var link = (linkField) ? "<a href='" + linkField.value + "'>" + item.name + "</a>" : item.name;
+
+                liString = "<li data-jstree='{\"icon\" : \"/js/icons/doc_01.png\"}'>" + link;
             }
+
+            //item type = snippet-1
             if(item.type.toLowerCase() == 'snippet-1') {
                 liString = "<li data-jstree='{\"icon\" : \"/js/icons/doc_02.png\"}'>" + item.name;
             }
+
             nodesHTML += liString;
             //console.log(item.name);
             //if item has child nodes, then list them
