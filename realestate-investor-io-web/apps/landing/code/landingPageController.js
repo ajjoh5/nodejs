194f73a76e0d0ab681741a7a8c88b70dffa8b02d
Adam Johnstone <adam.johnstone@igeniusgroup.com>
2016-02-22 23:01:08 +1100

    rea web: - Added query and home screen

--- realestate-investor-io-web/apps/landing/code/landingPageController.js
+++ realestate-investor-io-web/apps/landing/code/landingPageController.js
@@ -6,7 +6,18 @@ var landingPageController = function(app) {
         var viewParams = {
             layout : __dirname + '/../views/layouts/layout',
             viewFile : __dirname + '/../views/index',
-            message : 'NPM World'
+            'action-button' : 'BUY NOW'
+        };
+
+        res.render(viewParams.viewFile, viewParams);
+    });
+
+    app.get('/deals-home', function(req, res) {
+
+        var viewParams = {
+            layout : __dirname + '/../views/layouts/layout',
+            viewFile : __dirname + '/../views/deals-home',
+            'action-button' : '<span class="glyphicon glyphicon-user" aria-hidden="true"></span> Logout'
         };
 
         res.render(viewParams.viewFile, viewParams);
