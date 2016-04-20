f8a69fce88bee464d463370274f591310200d004
Adam Johnstone <adam.johnstone@igeniusgroup.com>
2016-04-20 20:27:39 +1000

    Particle-io - Added mongoldb insert (over nedb)

--- particle-io/app/lib/particle-data.js
+++ particle-io/app/lib/particle-data.js
@@ -1,19 +1,38 @@
 var ParticleData = function(options, particle) {
-    var Datastore = require('nedb');
-    var format = require('string-format');
 
-    //Init neDB JSON storage
-    var db = {};
-    db.particles = new Datastore({ filename: format('{0}/data/{1}.db', __base, options.group), autoload: true });
+    // var Datastore = require('nedb');
+    // var format = require('string-format');
+    //
+    // //Init neDB JSON storage
+    // var db = {};
+    // db.particles = new Datastore({ filename: format('{0}/data/{1}.db', __base, options.group), autoload: true });
+    //
+    // if (particle) {
+    //     db.particles.insert(particle, function (err, newDoc) {
+    //         if(err) {
+    //             //error handling
+    //         }
+    //         else {
+    //             //successful
+    //         }
+    //     });
+    // }
 
-    if (particle) {
-        db.particles.insert(particle, function (err, newDoc) {
-            if(err) {
-                //error handling
-            }
-            else {
-                //successful
-            }
+    if(particle) {
+        var MongoClient = require('mongodb').MongoClient;
+        var assert = require('assert');
+
+        // Connection URL
+        var url = 'mongodb://192.168.99.100:27017/test';
+        // Use connect method to connect to the Server
+        MongoClient.connect(url, function(err, db) {
+
+            db.collection('particles').insert(particle, function(err, result) {
+                console.log(result);
+                //if error, add this to database for processing later - like a queue!!
+                assert.equal(1, err, 'Error inserting record into mongodb.');
+                db.close();
+            });
         });
     }
 };
