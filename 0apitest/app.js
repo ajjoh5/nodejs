785601a9cf53d31aa2532fbc58051772168af52f
Adam Johnstone <adam.johnstone@igeniusgroup.com>
2016-04-27 22:16:15 +1000

    Fixed bad logging practices in api-proxy

--- 0apitest/app.js
+++ 0apitest/app.js
@@ -5,9 +5,42 @@ var app = express();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.post('/post', function (req, res) {
+app.get('/get/error', function (req, res) {
 
-    console.log('Post received');;
+    console.log('Get ERROR received');
+
+    res.status(500).send('Error this bad stuff happened.');
+});
+
+app.post('/post/error', function (req, res) {
+
+    console.log('Post ERROR received');
+    var reqBack = {
+        headers : req.headers,
+        body : req.body
+    };
+
+    console.log(reqBack);
+
+    res.status(500).send('Error this bad stuff happened.');
+});
+
+app.post('/post/web', function (req, res) {
+
+    console.log('Post WEB received');
+    var reqBack = {
+        headers : req.headers,
+        body : req.body
+    };
+
+    console.log(reqBack);
+
+    res.status(200).send('<html><p>This is a webpage response</p></html>');
+});
+
+app.post('/?*', function (req, res) {
+
+    console.log('Post received');
 
     var reqBack = {
         headers : req.headers,
@@ -19,9 +52,9 @@ app.post('/post', function (req, res) {
     res.send(reqBack);
 });
 
-app.get('/get', function (req, res) {
+app.get('/?*', function (req, res) {
 
-    console.log('Get received');;
+    console.log('Get received');
 
     var json = {
         name : 'Adam Johnstone',
--- api-proxy/apps/zzGeneric/code/zzGenericController.js
+++ api-proxy/apps/zzGeneric/code/zzGenericController.js
@@ -10,36 +10,21 @@ var zzGenericController = function(app) {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
+    // Favicon route
+    app.use('*/favicon.ico', function(req, res, next) {
+        res.sendFile(__base + '/favicon.ico');
+    });
+
     // Generic Catch All - GET METHOD
     app.get('/?*', function(req, res) {
 
         var urlParts = url.parse(req.url, true);
         var urlPath = urlParts.path;
-        var newUrl = 'http://localhost:5050' + urlPath;
+        //var newUrl = 'http://localhost:5050' + urlPath;
+        var newUrl = 'http://webapi.henley.com.au' + urlPath;
         var start = new Date();
 
-        //Time how long url takes to serve
-        //console.time(format('[v1 - default route - {0}]', urlPath));
-
         request.get(newUrl, function(error, response, body) {
-
-            var json = {};
-
-            // var particle = {
-            //     "type": "info",
-            //     "group": "red leader",
-            //     "data": {
-            //         "cartype": "gokart",
-            //         "cylinders": "1"
-            //     },
-            //     "name": "big daddy"
-            // };
-
-            var logEntry = {
-                type: 'info',
-                group: 'apiproxy.get',
-                request : {}
-            };
 
             //Set the response to whatever we get back (include all options)
             res.set(response.headers);
@@ -48,30 +33,63 @@ var zzGenericController = function(app) {
             var responseTime = new Date() - start;
 
             if(!error && response.statusCode == 200) {
+                var message = '';
                 var contentType = response.headers['content-type'];
                 if(contentType.indexOf('application/json') > -1) {
                     var json = JSON.parse(body);
-                    var message = (!json.ResponseMessage) ? '' : json.ResponseMessage;
-                    logEntry.request = { handler : 'zzGeneric', type : 'GET', httpStatusCode : response.statusCode, path : urlPath, exTime : responseTime, message : message };
-
-                    var hookOptions = {
-                        url: 'https://162.243.104.143/api/particles/new',
-                        method: 'POST',
-                        body : JSON.stringify(logEntry.request)
-                    };
-                    request(hookOptions);
+                    message = (!json.ResponseMessage) ? '' : json.ResponseMessage;
                 }
+
+                var logEntry = {
+                    type: 'info',
+                    group: 'apiproxy.get',
+                    handler : 'zzGeneric',
+                    httpMethod : 'GET',
+                    httpStatusCode : response.statusCode,
+                    path : urlPath,
+                    exTime : responseTime,
+                    message : message
+                };
+
+                var options = {
+                    url: 'http://162.243.104.143/api/particles/new',
+                    method: 'POST',
+                    json : true,
+                    body : logEntry,
+                    headers : {
+                        'authorization' : 'Bearer ff1bc4174529248c949e04d601fecc2f7c7dde5a32367ebff5f559b0fcf2615a'
+                    }
+                };
+
+                request(options, function(error, response, body) {});
             }
             else {
-                logEntry.type = 'error';
-                logEntry.request = { handler : 'zzGeneric', type : 'GET', httpStatusCode : response.statusCode, path : urlPath, exTime : responseTime, message : error };
 
-                var hookOptions = {
-                    url: 'https://162.243.104.143/api/particles/new',
-                    method: 'POST',
-                    body : JSON.stringify(logEntry.request)
+                //Set error to be what's returned in body, if anywhere in the body, contains the string = 'error'
+                var errorMessage = (body && body.toLowerCase().indexOf('error') > -1) ? body : '-';
+
+                var logEntry = {
+                    type: 'error',
+                    group: 'apiproxy.get',
+                    handler : 'zzGeneric',
+                    httpMethod : 'GET',
+                    httpStatusCode : response.statusCode,
+                    path : urlPath,
+                    exTime : responseTime,
+                    message : errorMessage
                 };
-                request(hookOptions);
+
+                var options = {
+                    url: 'http://162.243.104.143/api/particles/new',
+                    method: 'POST',
+                    json : true,
+                    body : logEntry,
+                    headers : {
+                        'authorization' : 'Bearer ff1bc4174529248c949e04d601fecc2f7c7dde5a32367ebff5f559b0fcf2615a'
+                    }
+                };
+
+                request(options);
             }
 
             res.end();
@@ -108,14 +126,6 @@ var zzGenericController = function(app) {
 
         request(options, function(error, response, body) {
 
-            //init response variables
-            //Set error to be what's returned in body, if anywhere in the body, contains the string = 'error'
-            var errorMessage = (!body && !body.toLowerCase().indexOf('error') > -1) ? '-' : body;
-            var logEntry = {
-                sourceType : 'zzGeneric',
-                request : {}
-            };
-
             //Set the response to whatever we get back (include all options)
             res.set(response.headers);
             res.send(body);
@@ -126,28 +136,64 @@ var zzGenericController = function(app) {
             //Determine what to log
             if (!error && response.statusCode == 200) {
 
+                var message = '';
                 var responseContentType = response.headers['content-type'];
-                if (responseContentType.indexOf('application/json') > -1) {
-                    var message = (!body.ResponseMessage) ? '' : body.ResponseMessage;
-                    logEntry.request = { level : 'Info', date : dateFormat(new Date(), 'dd-mm-yyyy h:MM:ss TT'), type : 'POST', httpStatusCode : response.statusCode, path : urlPath, exTime : responseTime, message : message };
 
-                    var hookOptions = {
-                        url: 'https://zapier.com/hooks/catch/294068/ubqxjb',
-                        method: 'POST',
-                        body : JSON.stringify(logEntry.request)
-                    };
-                    request(hookOptions);
+                //If return was json, get response message, else handle normally
+                if (responseContentType.indexOf('application/json') > -1) {
+                    message = (!body.ResponseMessage) ? '' : body.ResponseMessage;
                 }
+
+                var logEntry = {
+                    type: 'info',
+                    group: 'apiproxy.post',
+                    handler : 'zzGeneric',
+                    httpMethod : 'POST',
+                    httpStatusCode : response.statusCode,
+                    path : urlPath,
+                    exTime : responseTime,
+                    message : message
+                };
+
+                var options = {
+                    url: 'http://162.243.104.143/api/particles/new',
+                    method: 'POST',
+                    json : true,
+                    body : logEntry,
+                    headers : {
+                        'authorization' : 'Bearer ff1bc4174529248c949e04d601fecc2f7c7dde5a32367ebff5f559b0fcf2615a'
+                    }
+                };
+
+                request(options);
             }
             else {
-                logEntry.request = { level : 'Error', date : dateFormat(new Date(), 'dd-mm-yyyy h:MM:ss TT'), type : 'POST', httpStatusCode : response.statusCode, path : urlPath, exTime : responseTime, message : errorMessage };
 
-                var hookOptions = {
-                    url: 'https://zapier.com/hooks/catch/294068/ubqxjb',
-                    method: 'POST',
-                    body : JSON.stringify(logEntry.request)
+                //Set error to be what's returned in body, if anywhere in the body, contains the string = 'error'
+                var errorMessage = (body && body.toLowerCase().indexOf('error') > -1) ? body : '-';
+
+                var logEntry = {
+                    type: 'error',
+                    group: 'apiproxy.post',
+                    handler : 'zzGeneric',
+                    httpMethod : 'POST',
+                    httpStatusCode : response.statusCode,
+                    path : urlPath,
+                    exTime : responseTime,
+                    message : errorMessage
                 };
-                request(hookOptions);
+
+                var options = {
+                    url: 'http://162.243.104.143/api/particles/new',
+                    method: 'POST',
+                    json : true,
+                    body : logEntry,
+                    headers : {
+                        'authorization' : 'Bearer ff1bc4174529248c949e04d601fecc2f7c7dde5a32367ebff5f559b0fcf2615a'
+                    }
+                };
+
+                request(options);
             }
 
             //End response
